7ef00927c2827e81d20e27bc70e747529b94ba1d
Federico Blengio <fedeeblengio@gmail.com>
2024-10-28 18:30:08 -0300

    update

--- packages/fedeblengio/jetpack/src/Providers/JetpackServiceProvider.php
+++ packages/fedeblengio/jetpack/src/Providers/JetpackServiceProvider.php
@@ -8,12 +8,10 @@ class JetpackServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Cargar rutas, vistas y migraciones
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../Views', 'jetpack');
         $this->loadMigrationsFrom(__DIR__.'/../Migrations');
 
-        // Publicar archivos de configuración
         $this->publishes([
             __DIR__.'/../Config/jetpack.php' => config_path('jetpack.php'),
         ], 'config');
--- packages/fedeblengio/jetpack/src/routes/web.php
+++ packages/fedeblengio/jetpack/src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 use Fedeblengio\Jetpack\Controllers\JetpackController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/jetpack', [JetpackController::class, 'index']);
