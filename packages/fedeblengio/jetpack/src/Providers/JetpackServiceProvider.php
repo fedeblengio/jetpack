5ad5ccf832733e33063aee3809e053c4d29a26b3
Federico Blengio <fedeeblengio@gmail.com>
2024-10-28 21:50:04 -0300

    add routes

--- packages/fedeblengio/jetpack/src/Providers/JetpackServiceProvider.php
+++ packages/fedeblengio/jetpack/src/Providers/JetpackServiceProvider.php
@@ -8,19 +8,27 @@ class JetpackServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../Views', 'jetpack');
-//        $this->loadMigrationsFrom(__DIR__.'/../Migrations');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/jetpack/custom.php');
+        $this->loadViewsFrom(__DIR__ . '/../Views', 'jetpack');
+
+
+
 
         $this->publishes([
-            __DIR__.'/../Config/jetpack.php' => config_path('jetpack.php'),
+            __DIR__ . '/../routes/jetpack/custom.php' => base_path('routes/jetpack/custom.php'),
+        ], 'jetpack-routes');
+
+
+        $this->publishes([
+            __DIR__ . '/../Config/jetpack.php' => config_path('jetpack.php'),
         ], 'config');
+
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../Config/jetpack.php', 'jetpack'
+            __DIR__ . '/../Config/jetpack.php', 'jetpack'
         );
     }
 }
